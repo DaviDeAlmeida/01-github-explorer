30d87c0bd452d93c9cdaa7fefe0e37411f706151
Davi Cardoso <davi.almeida.c@gmail.com>
2022-02-11 20:45:31 -0300

    added component state

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export function Counter() {
+    const [count, setCount] = useState(0);
+    function increment(){
+        console.log("Entrou");
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button type="button" onClick={increment}>Increment</button>
+        </div>
+    );
+}
