c0de91ae5400f42de2b7d520eb2ff32361733d10
Davi Cardoso <davi.almeida.c@gmail.com>
2022-02-13 18:07:23 -0300

    add fetch method to github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,5 @@
 import { RepositoryItem } from "./RepositoryItem";
-
+import { useEffect, useState } from "react";
 
 import '../styles/repository.scss';
 
@@ -10,6 +10,17 @@ const repository = {
 };
 
 export function RepositoryList() {
+const [repositories, setRepositories] = useState([])
+
+useEffect(() => {
+  fetch('https://api.github.com/orgs/rocketseat/repos')
+  .then(response => response.json())
+  .then(data => setRepositories(data))
+
+}, [repositories]); // Sempre colocar o segundo parametro
+
+// Todo código aqui pode ser renderizado novamente caso o estado mude
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
